c32f3ebb2af8b926353589dfef330d1dc9d03516
thomasmburke <tburke@bu.edu>
2019-09-11 13:54:45 -0400

    Updates

---
+++ precache-manifest.ab16712021a5c32c8dbbbbd8f1645d6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "69e982dbb316f37d796b",
+    "url": "/thomasmburke.github.io/static/css/main.f3f3a3b2.chunk.css"
+  },
+  {
+    "revision": "69e982dbb316f37d796b",
+    "url": "/thomasmburke.github.io/static/js/main.69e982db.chunk.js"
+  },
+  {
+    "revision": "1cddf11d618a34493b21",
+    "url": "/thomasmburke.github.io/static/js/runtime~main.1cddf11d.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/thomasmburke.github.io/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "4864d129faee12229f1bc38f7197584e",
+    "url": "/thomasmburke.github.io/index.html"
+  }
+];
